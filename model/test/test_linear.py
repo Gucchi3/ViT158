4e18d7bdbde83ff0a94c40c15e01a06bfcff4d94
gucchi3 <nnykb-k@outlook.jp>
2026-02-14 01:24:00 +0900

    8bit 量子化Linear

--- model/test/test_linear.py
+++ model/test/test_linear.py
@@ -16,12 +16,13 @@ except ImportError:
 
 
 
-# ── TerLinearCUDA ───────────────────────────────────────────────────────
+# ── TerLinear ───────────────────────────────────────────────────────────
 class TerLinear(nn.Linear):
-    def __init__(self, in_features, out_features, bias=None):
+    def __init__(self, in_features, out_features, bias=True, use_layer_norm=True):
         super().__init__(in_features, out_features, bias)
         self.quantizer = Quantizer()
-        self.norm = nn.LayerNorm(in_features)
+        self.use_layer_norm = use_layer_norm
+        self.norm = nn.LayerNorm(in_features) if use_layer_norm else nn.Identity()
 
     def forward(self, x):
         x = self.norm(x)
@@ -31,6 +32,27 @@ class TerLinear(nn.Linear):
         x_quant = x + (x_q - x).detach()
         # weight
         w_q, _ = self.quantizer.to_ter_per_tensor(w, as_float=True)
+        w_quant = w + (w_q - w).detach()
+        # return
+        return F.linear(x_quant, w_quant, self.bias)
+
+
+# ── Q_Linear ───────────────────────────────────────────────────────────
+class Q_Linear(nn.Linear):
+    def __init__(self, in_features, out_features, bias=True, use_layer_norm=True):
+        super().__init__(in_features, out_features, bias)
+        self.quantizer = Quantizer()
+        self.use_layer_norm = use_layer_norm
+        self.norm = nn.LayerNorm(in_features) if use_layer_norm else nn.Identity()
+
+    def forward(self, x):
+        x = self.norm(x)
+        w = self.weight
+        # activation
+        x_q, _ = self.quantizer.to_bit_per_tensor(x, bit=8, as_float=True, unsigned=False)
+        x_quant = x + (x_q - x).detach()
+        # weight
+        w_q, _ = self.quantizer.to_bit_per_tensor(w, bit=8, as_float=True, unsigned=False)
         w_quant = w + (w_q - w).detach()
         # return
         return F.linear(x_quant, w_quant, self.bias)
